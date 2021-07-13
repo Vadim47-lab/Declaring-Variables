b80899e3f26b18199e2cac235361aa12f1dc8090
Vadim Stepanov <vadstep777@mail.ru>
2021-07-13 14:13:51 +0700

    Decler peremen

---
+++ Declaring Variables/Declaring Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declaring_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //целочисленные переменные
+            byte b1 = 10;
+            sbyte sb = 20;
+            short sh = 30;
+            ushort ush = 15;
+            int i = 1;
+            uint ui = 3;
+            long lng = 1000;
+            ulong ulng = 100;
+            //переменные с плавающей точкой
+            float fl = 1.5f;
+            double db = 3.4f;
+            //символьный тип
+            char chr = '!';
+            //строковый тип
+            string str = "Привет, какие планы на сегодня";
+            //логический тип
+            bool bl = false;
+        }
+    }
+}
